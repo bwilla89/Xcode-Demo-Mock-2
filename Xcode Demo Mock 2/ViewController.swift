5a77a132c9b3df37abae57f0206bcea73c3e781c
Brian Williams <bwilla89@gmail.com>
2019-09-11 21:40:52 -0400

    Initial Commit

---
+++ Xcode Demo Mock 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode Demo Mock 2
+//
+//  Created by Brian williams on 9/11/19.
+//  Copyright © 2019 Brian williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
